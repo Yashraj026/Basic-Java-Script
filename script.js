97623e6624aa9c8a9f25526a7c48ccb1f0d5b330
Yash Raj <yashraj9227@gmail.com>
2026-02-05 16:12:12 +0530

    Added

---
+++ script.js
@@ -0,0 +1,31 @@
+const box1 = document.getElementById("box-1");
+box1.addEventListener("click", () => {
+  box1.style.color = "white ";
+  box1.style.backgroundColor = "red";
+});
+
+const box2 = document.getElementById("box-2");
+box2.addEventListener("click", () => {
+  box2.style.color = "white ";
+  box2.style.backgroundColor = "blue";
+});
+
+const box3 = document.getElementById("box-3");
+box3.addEventListener("click", () => {
+  box3.style.color = "white ";
+  box3.style.backgroundColor = "green";
+});
+
+const box4 = document.getElementById("box-4");
+box4.addEventListener("click", () => {
+  box4.style.color = "balck";
+  box4.style.backgroundColor = "yellow ";
+});
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let input = document.getElementById("in").value;
+  let h1 = document.getElementById("head");
+  h1.innerHTML = `Hello, ${input}`;
+});
